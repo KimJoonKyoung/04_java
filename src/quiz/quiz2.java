f9a01421872879ec9f6938cee07cdf543ca107cf
Kim Joon Kyoung <gang_3@naver.com>
2024-06-19 17:53:12 +0900

    참조 타입

---
+++ src/quiz/quiz2.java
@@ -0,0 +1,12 @@
+package quiz;
+
+public class quiz2 {
+    public static void main(String[] args) {
+        float a = 5;
+        float b = 10;
+        float h = 7;
+
+        float result = (a+b)*h/2;
+        System.out.println(result);
+    }
+}
